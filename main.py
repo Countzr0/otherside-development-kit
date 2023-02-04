01b131aa43090ff6b4d520adbba84eb82afe1529
Countzr0 <countzer0@yugalabs.io>
2023-02-03 20:04:23 -0800

    Initial upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# ! /usr/bin/env python3
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def greetings_program(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Greetings program: {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    greetings_program('Count zer0')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
